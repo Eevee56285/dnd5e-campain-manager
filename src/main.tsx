d74b60a1cf8a938b36850c06e08c40e2af51ba88
Eevee56285 <woodfamily1107@gmail.com>
2025-10-26 09:33:39 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { UserProvider } from './lib/UserContext';   // ← add this
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UserProvider>          {/* ← wrap here */}
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
